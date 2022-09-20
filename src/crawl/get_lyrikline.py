d752e1343df67a3d363a1c3430e5a80a762aac4b
Yuchen Eleanor Jiang <eleanorjiang630@gmail.com>
2022-09-20 11:28:06 +0200

    upload the lyrikline script

--- src/crawl/get_lyrikline.py
+++ src/crawl/get_lyrikline.py
@@ -9,8 +9,8 @@ from tqdm import tqdm
 import sys
 import time
 sys.path.append("src")
-from utils import json_dumpa, delete_file, create_crawl_dir, json_reada
-from crawl.utils_lang import safe_langdetect
+# from utils import json_dumpa, delete_file, create_crawl_dir, json_reada
+#from crawl.utils_lang import safe_langdetect
 
 def process_poem_body(find_all_result):
     pass
@@ -18,28 +18,28 @@ def process_poem_body(find_all_result):
 def url_to_poem(url):
     pass
 
+def get_author():
+    url = "https://www.lyrikline.org/en/poems/1118-7993"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
+    r = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(r)
+    soup = BeautifulSoup(html)
+    links = soup.find_all('a')
+
+    print("end")
+    return
+
 
 def get_all():
     # remove previous iteration if exists
-    delete_file("crawl/lyrikline_txt.jsonl")
-    delete_file("crawl/lyrikline_meta.jsonl")
+    # delete_file("crawl/lyrikline_txt.jsonl")
+    # delete_file("crawl/lyrikline_meta.jsonl")
 
-    print("Phase 1: collecting links")
-
-    for lang in ["cs", "de", "zh", "es", "it", "ru", "ar"]:
+    for lang in ["zh"]:  # ["cs", "de", "zh", "es", "it", "ru", "ar"]
         pass
 
         # throttle to not trigger ip limits
         time.sleep(1.1)
 
 if __name__ == "__main__":
-    create_crawl_dir()
-    args = argparse.ArgumentParser()
-    args.add_argument("--metadata", default="")
-    args = args.parse_args()
-
-    if len(args.metadata) == 0 or not Path(args.metadata).is_file():
-        print("WARNING: Metadata not loaded, crawling anew. Resulting dataset won't be compatible.")
-        get_all()
-    else:
-        pass
+    get_author()
